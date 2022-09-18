a81c71ce6e3d433d48f2a4c780fc0d8a05834560
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-09-18 11:04:39 +0100

    chore:
    Created Business Serializer

---
+++ business/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from business.models import Business
+
+
+class BusinessSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Business
+        fields = "__all__"
