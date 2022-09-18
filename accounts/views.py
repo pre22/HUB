0aae89432ae2cc5b7d3f7e12179773942bf9a542
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-09-18 11:03:37 +0100

    update:
    added doc string to ReviewView class

--- accounts/views.py
+++ accounts/views.py
@@ -3,5 +3,6 @@ from accounts.models import Reviews
 from accounts.serializers import ReviewSerializer
 
 class ReviewView(generics.ListCreateAPIView):
+    ''' Handles Create-Review Form and also List the Reviews a Business has'''
     queryset = Reviews.objects.all()
     serializer_class = ReviewSerializer
