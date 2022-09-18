38c24608eaaf4164b051824f7a399a8ee0f5b8f1
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-09-18 11:07:16 +0100

    chores:
    created routes for services app

--- services/urls.py
+++ services/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from services.views import OrderView, ServicePackageView
 
 urlpatterns = [
-    
+    path("order/", OrderView.as_view()),
+    path("package/", ServicePackageView.as_view())
 ]
