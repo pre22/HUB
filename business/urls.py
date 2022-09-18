a7789dfd8beb7375d30d43c6783d7a97b30536cb
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-09-18 11:05:48 +0100

    chores:
    created routes forbusiness views

--- business/urls.py
+++ business/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from business.views import BusinessList, AboutBusiness
 
 urlpatterns = [
-    
+    path("list/", BusinessList.as_view()),
+    path("about/", AboutBusiness.as_view()),
 ]
