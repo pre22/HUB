c216e361efc45307d86e7921c0b877db9e64894f
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-09-18 09:49:43 +0100

    chore:
    created UserCreateSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,15 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer
 from accounts.models import Reviews
+
+User = get_user_model()
+
+class UserCreateSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model =  User
+        fields = ("id", "email", "firstname", "middlename", "lastname", "password")
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
