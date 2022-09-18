d245e751572fa5ba1dcfaf9b28b319fc66e6074a
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-09-18 11:06:43 +0100

    chores
    created views for service serializers

--- services/views.py
+++ services/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from services.models import Order, ServicePackage
+from services.serializers import OrderSerializer, ServicePackageSerializer
 
-# Create your views here.
+class OrderView(generics.ListCreateAPIView):
+    ''' Handles Create Order form and Order History '''
+    queryset = Order.objects.all()
+    serialzer_class = OrderSerializer
+
+class ServicePackageView(generics.ListCreateAPIView):
+    ''' Handles Create Service Package form for Business and Business Service List '''
+    queryset = ServicePackage.objects.all()
+    serializer_class = ServicePackageSerializer
+
