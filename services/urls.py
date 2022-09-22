e6950f9fede08b332ec34678f0a9508a94100f1b
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-09-22 02:11:50 +0100

    chore: created order route

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from services.views import OrderView, OrderDetailView, ServicePackageView, ServicePackageDetailView
+from services.views import OrderView, OrderDetailView, OrderUpdateView, ServicePackageView, ServicePackageDetailView
 
 urlpatterns = [
     path("order/", OrderView.as_view()),
-    path("order/<int:pk/", OrderDetailView.as_view()),
+    path("order/<int:pk>/", OrderDetailView.as_view()),
+    path("order/update/<int:pk>/", OrderUpdateView.as_view()),
     path("package/", ServicePackageView.as_view()),
     path("package/details/<int:pk>/", ServicePackageDetailView.as_view())
 ]
